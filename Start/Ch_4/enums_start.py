5b221889c456af06cf413b464943172509fd5dcd
Joe <77728651+Jingham2510@users.noreply.github.com>
2023-11-28 21:06:14 +0000

    chap 4 first 2 chapters

--- Start/Ch_4/enums_start.py
+++ Start/Ch_4/enums_start.py
@@ -1,11 +1,31 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # define enumerations using the Enum base class
 
+from enum import Enum, unique, auto
 
 # TODO: enums have human-readable values and types
+#Unique decorator disables values from having the same value
+@unique
+class Fruit(Enum):
+    APPLE = 1
+    BANANA = 2
+    ORANGE = 3
+    TOMATO = 4
+    RED_DELICIOUS = 12
+    #Auto assigns new value
+    PEAR = auto()
 
 # TODO: enums have name and value properties
+print(Fruit.APPLE)
+print(type(Fruit.APPLE))
+print(repr(Fruit.APPLE))
 
+print(Fruit.APPLE.name, Fruit.APPLE.value)
 # TODO: print the auto-generated value
+print(Fruit.PEAR.value)
 
 # TODO: enums are hashable - can be used as keys
+my_fruits = {}
+my_fruits[Fruit.BANANA] = "Come Mr. Tally Man"
+
+print(my_fruits[Fruit.BANANA])
--- Start/Ch_4/objectstrs_start.py
+++ Start/Ch_4/objectstrs_start.py
@@ -9,9 +9,18 @@ class Person():
         self.age = 25
 
     # TODO: use __repr__ to create a string useful for debugging
+    def __repr__(self):
+        return f"<Person Class = - fname: {self.fname}, lname: {self.lname}, age: {self.age}>"
+
 
     # TODO: use str for a more human-readable string
+    def __str__(self):
+        return f"Person {self.fname} {self.lname} is {self.age} years old"
 
+    #Determines the byte representation of the class
+    def __bytes__(self):
+        val = f"Person:{self.fname}:{self.lname}:{self.age}"
+        return bytes(val.encode("utf-8"))
 
 # create a new Person object
 cls1 = Person()
@@ -20,3 +29,5 @@ cls1 = Person()
 print(repr(cls1))
 print(str(cls1))
 print(f"Formatted: {cls1}")
+
+print(bytes(cls1))
