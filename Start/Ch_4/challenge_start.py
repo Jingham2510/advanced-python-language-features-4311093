1d11274f793bdb48ea6571cba743db88ce9964c8
Joe <77728651+Jingham2510@users.noreply.github.com>
2023-12-04 20:27:12 +0000

    Chapter 4 finish

--- Start/Ch_4/challenge_start.py
+++ Start/Ch_4/challenge_start.py
@@ -5,20 +5,16 @@
 # Challenge:
 # Given a class that represents a Book with various properties such
 # as title, author, pagecount, etc:
-# 1) Implement the __repr__ and __str__ functions to output:
-#   str:  "(title) by (author): (pagecount), (cover), (price)"
-#   repr: "<Book:title:author:pages:cover:antique:genre:price>"
-# 2) Implement the comparison methods to allow comparing books based
-#   on pagecount.
-# 3) Implement an enum that represents the type of the book cover. The
-#   allowable cover types are "hard" and "paperback". Replace the existing
-#   "Hard" and "paperback" strings with the enum.
-# 4) Implement an "adjustedprice" computed attribute - books that are antiques
-#   have a 10.00 surcharge on their price, Paperback books get a 2.00 discount
+
+
 # 5) Successfully execute the sample code provided below.
+
+from enum import Enum
 
 
 class Book():
+
+    
     def __init__(self, title, author, pages, cover, antique, price):
         self.title = title
         self.author = author
@@ -28,24 +24,56 @@ class Book():
         self.price = price
 
     # TODO: Implement the str and repr functions
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}: {self.pages}, {self.cover}, {self.price}"
+
+    def __repr__(self):
+        return f"<Book:{self.title}:{self.author}:{self.pages}:{self.cover}:{self.antique}:{self.price}"
 
     # TODO: Implement the adjustedprice attribute
+    def __getattr__(self, attr):
+        if attr == "adjustedprice":
+            price = self.price
+            if self.antique:
+               price = price + 10.00
+            if self.cover == cover_type.PAPERBACK:
+                price = price - 2.00
+            return price
+
+        else:
+            raise AttributeError("Invalid attribute")
+
 
     # TODO: Implement comparisons <, >, <=, >=
+    def __gt__(self, other):
+        return self.pages > other.pages
+    
+    def __ge__(self, other):
+        return self.pages >= other.pages 
+    
+    def __lt__(self, other):
+        return self.pages < other.pages 
+    
+    def __le__(self, other):
+        return self.pages <= other.pages 
 
 
 # TODO: Implement the Hard/Paperback Enum
+class cover_type(Enum):
+    HARD = 0
+    PAPERBACK = 1
 
 
 books = [
-    Book("War and Peace", "Leo Tolstoy", 1225, "Hard", True, 29.95),
-    Book("Brave New World", "Aldous Huxley", 311, "Paperback", True, 32.50),
-    Book("Crime and Punishment", "Fyodor Dostoevsky", 492, "Hard", False, 19.75),
-    Book("Moby Dick", "Herman Melville", 427, "Paperback", True, 22.95),
-    Book("A Christmas Carol", "Charles Dickens", 66, "Hard", False, 31.95),
-    Book("Animal Farm", "George Orwell", 130, "Paperback", False, 26.95),
-    Book("Farenheit 451", "Ray Bradbury", 256, "Hard", True, 28.95),
-    Book("Jane Eyre", "Charlotte Bronte", 536, "Paperback", False, 34.95)
+    Book("War and Peace", "Leo Tolstoy", 1225, cover_type.HARD, True, 29.95),
+    Book("Brave New World", "Aldous Huxley", 311, cover_type.PAPERBACK, True, 32.50),
+    Book("Crime and Punishment", "Fyodor Dostoevsky", 492, cover_type.HARD, False, 19.75),
+    Book("Moby Dick", "Herman Melville", 427, cover_type.PAPERBACK, True, 22.95),
+    Book("A Christmas Carol", "Charles Dickens", 66, cover_type.HARD, False, 31.95),
+    Book("Animal Farm", "George Orwell", 130, cover_type.PAPERBACK, False, 26.95),
+    Book("Farenheit 451", "Ray Bradbury", 256, cover_type.HARD, True, 28.95),
+    Book("Jane Eyre", "Charlotte Bronte", 536, cover_type.PAPERBACK, False, 34.95)
 ]
 
 # TEST CODE
--- Start/Ch_4/compattrs_start.py
+++ Start/Ch_4/compattrs_start.py
@@ -9,24 +9,41 @@ class MyColor():
         self.blue = 100
 
     # TODO: use getattr to dynamically return a value
-    # def __getattr__(self, attr):
-    #     pass
+    def __getattr__(self, attr):
+        if attr == "rgbcolour":
+            return(self.red, self.green, self.blue)
+        elif attr == "hexcolour":
+            return f"#{self.red:02x}{self.green:02x}{self.blue:02x}"
+        else:
+            raise AttributeError(f"{attr} is not valid")
 
     # TODO: use setattr to dynamically return a value
-    # def __setattr__(self, attr, val):
-    #     pass
+    def __setattr__(self, attr, val):
+         
+        if attr == "rgbcolour":
+            self.red = val[0]
+            self.green = val[1]
+            self.red = val[2]
+        else:
+            super().__setattr__(attr, val)
+        
 
     # TODO: use dir to list the available properties
-    # def __dir__(self):
-    #     pass
+    def __dir__(self):
+        return("rgbcolour", "hexcolour", "red", "green", "blue")
+    
 
 
 # create an instance of myColor
 cls1 = MyColor()
 # TODO: print the value of a computed attribute
-
+print(cls1.rgbcolour)
+print(cls1.hexcolour)
 # TODO: set the value of a computed attribute
-
+cls1.rgbcolour = (125, 200, 92)
+print(cls1.rgbcolour)
+print(cls1.hexcolour)
 # TODO: access a regular attribute
-
+print(cls1.red)
 # TODO: list the available attributes
+print(dir(cls1))
